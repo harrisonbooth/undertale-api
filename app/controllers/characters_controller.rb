23463870e70622abe7bd6218140b255687e675c8
Harrison Booth <harrisonbooth@ymail.com>
2017-04-28 15:05:06 +0100

    remove useless data from json and add cors gem to allow cross ogirin access

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,5 @@
 class CharactersController < ActionController::Base
   def index
-    render json: Character.all
+    render json: Character.all.to_json(except: [:created_at, :updated_at])
   end
 end
